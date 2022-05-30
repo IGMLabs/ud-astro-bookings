fd84f99bc5b61b3d8f45e9e22cccbef25a23202f
udiaz <uxia.diaz@igmweb.com>
2022-05-30 17:47:22 +0200

    sesion 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ud-astro-bookings';
+  public title = 'ud-astro-bookings';
+  public subtitle = 'welcome on board';
+  public author = 'udp';
+  public authorUrl = 'https://picocss.com/docs/';
+  public agencies = [
+    {
+      id: 'space-y',
+      name: 'Space Y',
+      range: 'Interplanetary',
+      status: 'Active',
+    },
+    {
+      id: 'green-origin',
+      name: 'Green Origin',
+      range: 'Orbital',
+      status: 'Active',
+    },
+    {
+      id: 'virgin-way',
+      name: 'Virgin Way',
+      range: 'Orbital',
+      status: 'Pending',
+    },
+  ];
+
+  public getAgenciesLength(){
+    return this.agencies.length;
+  }
+  public reload (){
+    this.reloading=true;
+    console.log ("Reloading");
+  }
+  public reloading =false;
 }
